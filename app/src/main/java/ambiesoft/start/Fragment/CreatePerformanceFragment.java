568029c292b6136ce71fe3e1b91ea10c8a23f638
bryanyhy <bryanyhy@hotmail.com>
2016-08-14 19:43:27 +1000

    Add choose location function in create performance Implement the Google Places API

--- app/src/main/java/ambiesoft/start/Fragment/CreatePerformanceFragment.java
+++ app/src/main/java/ambiesoft/start/Fragment/CreatePerformanceFragment.java
@@ -1,8 +1,12 @@
 package ambiesoft.start.Fragment;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +17,16 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+
 import ambiesoft.start.R;
 
 import static ambiesoft.start.Utility.AlertBox.showAlertBox;
@@ -20,15 +34,21 @@ import static ambiesoft.start.Utility.AlertBox.showAlertBox;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CreatePerformanceFragment extends Fragment{
+public class CreatePerformanceFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
+    private static final int PLACE_PICKER_REQUEST = 1;
+
+    private GoogleApiClient mGoogleApiClient;
     private boolean isStartup = true;
     private String selectedCategory;
+    private Double selectedLat;
+    private Double selectedLng;
 
     private EditText nameInput;
     private EditText descInput;
     private Spinner categorySpinner;
     private Button nextButton;
+    private Button locationButton;
 
 
     private static final String CATEGORY_TYPE[] = {"Instrument", "Dance", "Magic"};
@@ -52,12 +72,29 @@ public class CreatePerformanceFragment extends Fragment{
                 submit();
             }
         });
+        locationButton = (Button) view.findViewById(R.id.locationButton);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                chooseLocation();
+            }
+        });
         return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient
+                    .Builder(getContext())
+                    .addApi(Places.GEO_DATA_API)
+                    .addApi(Places.PLACE_DETECTION_API)
+                    .build();
+        }
+        mGoogleApiClient.connect();
+
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.category_array, android.R.layout.simple_spinner_item);
@@ -81,6 +118,31 @@ public class CreatePerformanceFragment extends Fragment{
         });
     }
 
+    public void chooseLocation() {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            startActivityForResult(builder.build(getActivity()), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, getActivity());
+                String locationName = place.getName().toString();
+                LatLng latLng = place.getLatLng();
+                selectedLat = latLng.latitude;
+                selectedLng = latLng.longitude;
+                Toast.makeText(getActivity(), "Lat: " + selectedLat + ", Lng: " + selectedLng, Toast.LENGTH_LONG).show();
+                locationButton.setText(locationName);
+            }
+        }
+    }
+
     public void submit() {
         String name = nameInput.getText().toString();
         String desc = descInput.getText().toString();
@@ -92,4 +154,18 @@ public class CreatePerformanceFragment extends Fragment{
         }
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
