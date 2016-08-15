0964e12741c7cbebe8af55c8435cc6f61d6f62d7
bryanyhy <bryanyhy@hotmail.com>
2016-08-15 21:54:27 +1000

    Saving the artwork JSON file in assets folder; Getting all the data from it, and draw artwork markers on map

---
+++ app/src/main/java/ambiesoft/start/dataclass/Artwork.java
@@ -0,0 +1,83 @@
+package ambiesoft.start.dataclass;
+
+/**
+ * Created by Bryanyhy on 15/8/2016.
+ */
+public class Artwork {
+
+    private String assetType;
+    private String name;
+    private String address;
+    private String artist;
+    private String artDate;
+    private Double lat;
+    private Double lng;
+
+    public Artwork() { }
+
+    public Artwork(String assetType, String name, String address, String artist, String artDate, Double lat, Double lng) {
+        this.assetType = assetType;
+        this.name = name;
+        this.address = address;
+        this.artist = artist;
+        this.artDate = artDate;
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public String getAssetType() {
+        return assetType;
+    }
+
+    public void setAssetType(String assetType) {
+        this.assetType = assetType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getArtDate() {
+        return artDate;
+    }
+
+    public void setArtDate(String artDate) {
+        this.artDate = artDate;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+}
--- app/src/main/java/ambiesoft/start/fragment/GoogleMapFragment.java
+++ app/src/main/java/ambiesoft/start/fragment/GoogleMapFragment.java
@@ -2,10 +2,14 @@ package ambiesoft.start.fragment;
 
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +19,24 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import ambiesoft.start.R;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import ambiesoft.start.R;
+import ambiesoft.start.dataclass.Artwork;
+
+import static ambiesoft.start.utility.JSON.loadJSONFromAsset;
 import static ambiesoft.start.utility.NetworkAvailability.isNetworkAvailable;
 
 /**
@@ -30,6 +47,7 @@ public class GoogleMapFragment extends Fragment implements OnMapReadyCallback {
     private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
     private static final int MY_PERMISSIONS_REQUEST_COARSE_LOCATION = 2;
     private GoogleMap mMap;
+    private ArrayList<Artwork> artworks;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,6 +59,8 @@ public class GoogleMapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        artworks = new ArrayList<>();
+        new SetupArtworkMarker().execute();
         MapFragment mapFragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         if (mapFragment == null) {
@@ -62,6 +82,47 @@ public class GoogleMapFragment extends Fragment implements OnMapReadyCallback {
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    // class used for setup the markers on artwork in City of Melbourne
+    private class SetupArtworkMarker extends AsyncTask<Void, Void, Void> {
+
+        // Achieve the JSON file on the artwork, and get necessary attributes from it
+        // Pass those values to a new Artwork object in an Artwork ArrayList
+        protected Void doInBackground(Void... voids) {
+            try {
+                // create a new JSONObject, by getting the artwork dataset in String format through a method in JSON Class
+                JSONObject jsonObj = new JSONObject(loadJSONFromAsset(getActivity()));
+                JSONArray data = jsonObj.getJSONArray("data");
+                for (int i = 0; i < data.length(); i++) {
+                    JSONArray specificArtwork = data.getJSONArray(i);
+                    String assetType = specificArtwork.getString(8);
+                    String name = specificArtwork.getString(9);
+                    String address = specificArtwork.getString(12);
+                    String artist = specificArtwork.getString(13);
+                    String artDate = specificArtwork.getString(15);
+                    JSONArray latLng = specificArtwork.getJSONArray(19);
+                    Double lat = latLng.getDouble(1);
+                    Double lng = latLng.getDouble(2);
+                    Artwork artwork = new Artwork(assetType, name, address, artist, artDate, lat, lng);
+                    artworks.add(artwork);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        // After the background process is finished, we are going to add marker for every artwork onto the map,
+        // by making use of their latitude and longitude
+        protected void onPostExecute(Void aVoid) {
+            Log.i("System.out","Size is " + artworks.size());
+            for (Artwork artwork: artworks) {
+                LatLng artworkMarker = new LatLng(artwork.getLat(), artwork.getLng());
+                mMap.addMarker(new MarkerOptions().title(artwork.getName()).position(artworkMarker)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+            }
         }
     }
 }
