e7eb665b68d13be1676eba3320e1c3ce235c02c3
Zelta <zeltawatt@gmail.com>
2016-08-16 22:34:57 +1000

    Add all markers

--- app/src/main/java/ambiesoft/start/fragment/GoogleMapFragment.java
+++ app/src/main/java/ambiesoft/start/fragment/GoogleMapFragment.java
@@ -8,15 +8,15 @@ import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
+
 import android.util.Log;
-import android.util.TypedValue;
+
 import android.view.Gravity;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
@@ -27,7 +27,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -65,17 +64,7 @@ public class GoogleMapFragment extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
-
-        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getFragmentManager().beginTransaction().replace(R.id.content_frame, new HomeFragment()).commit();
-            }
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_map, null, false);
     }
 
     @Override
