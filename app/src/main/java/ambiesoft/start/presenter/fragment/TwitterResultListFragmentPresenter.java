ad9c37038e7cd15765d2f6b8e6c2fe9d768e7010
Zelta <zeltawatt@gmail.com>
2016-09-21 16:15:12 +1000

    TWITTER WORKSSSSS!

--- app/src/main/java/ambiesoft/start/presenter/fragment/TwitterResultListFragmentPresenter.java
+++ app/src/main/java/ambiesoft/start/presenter/fragment/TwitterResultListFragmentPresenter.java
@@ -29,7 +29,7 @@ import ambiesoft.start.view.fragment.TwitterResultListFragment;
  */
 public class TwitterResultListFragmentPresenter {
 
-    private static final String HASHTAG = "#startinmelb";
+    private static final String HASHTAG = "#STARTMelb";
 //    #STARTinMelb
 
     private TwitterResultListFragment view;
@@ -44,9 +44,8 @@ public class TwitterResultListFragmentPresenter {
     }
 
     public void twitterResultListSetup() {
-        Log.i("System.out", "run twitter list");
         searchTimeline = new SearchTimeline.Builder()
-                .query(HASHTAG)
+                .query(HASHTAG).resultType(SearchTimeline.ResultType.RECENT)
                 .build();
 
 //        final UserTimeline userTimeline = new UserTimeline.Builder()
@@ -56,11 +55,6 @@ public class TwitterResultListFragmentPresenter {
         adapter = new TweetTimelineListAdapter.Builder(view.getActivity())
                 .setTimeline(searchTimeline)
                 .build();
-
-//        final TweetTimelineListAdapter timelineAdapter = new TweetTimelineListAdapter(view.getContext(), searchTimeline);
-
-//        view.tweetList.setAdapter(timelineAdapter);
-
         view.tweetList.setAdapter(adapter);
         view.tweetList.setNestedScrollingEnabled(true);
 
